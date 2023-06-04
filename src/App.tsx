3fd8c20bf362fcb13b44a9d117c7721ebcf000e7
1 <a1@MacBook-Air-1.local>
2023-06-04 10:48:14 +0300

    [add]: esLit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-console.log('asdasd');
+  const [count, setCount] = useState(0);
+  const fsdf = 23;
 
   return (
     <>
@@ -30,7 +30,7 @@ console.log('asdasd');
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
