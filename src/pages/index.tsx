a58d085dd0fc0e1095db88cb071533a16767527b
1 <a1@MacBook-Air-1.local>
2023-06-05 16:31:04 +0300

    crete: todo

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+// Либо использовать @loadable/component, в рамках туториала - некритично
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const TasksListPage = lazy(() => import("./tasks-list"));
+const TaskDetailsPage = lazy(() => import("./task-details"));
+
+export const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TasksListPage />} />
+      <Route path="/:taskId" element={<TaskDetailsPage />} />
+    </Routes>
+  );
+};
